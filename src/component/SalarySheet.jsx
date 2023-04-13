d014deb6f6aaf0d741457b77a9039657b4e9a5f7
noyon-podder <noyon.vai369@gmail.com>
2023-04-14 01:53:59 +0600

    table add this commit

---
+++ src/component/SalarySheet.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Table from "react-bootstrap/Table";
+import { useLoaderData } from 'react-router-dom';
+
+const SalarySheet = () => {
+    const salaryData = useLoaderData();
+  return (
+    <section className='p-5 '>
+      <Table responsive>
+        <thead className='bg-info'>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Salary</th>
+            <th>Date</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {salaryData.map((salary, i) => (
+            <tr key={salary._id}>
+              <td>{i + 1}</td>
+              <td>{salary.firstName}</td>
+              <td>{salary.lastName}</td>
+              <td>{salary.email}</td>
+              <td>{salary.salary}</td>
+              <td>{salary.date}</td>
+              <td className=''>
+                <button>Edit</button>
+                <button className='ms-4'>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </section>
+  );
+}
+
+export default SalarySheet
